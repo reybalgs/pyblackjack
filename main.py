89f198148b5e2606252c61730de0ce1435d0f60a
Aldo Rey Balagulan <mrawesomer34@gmail.com>
2012-09-03 13:20:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#
+# pyblackjack/main.py
+#
+# The main file. This will be the Python script to be executed in order for the
+# game to run.
+
+# Import everything from the games module
+from game import *
+
+printf 'Hello world!'
