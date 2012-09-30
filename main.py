6a9d1f3e351f8ac552b596599057d3ae1a9ef2f6
Aldo Rey Balagulan <mrawesomer34@gmail.com>
2012-09-30 14:18:14 +0800

    Added a hastily-made main menu

--- main.py
+++ main.py
@@ -7,4 +7,21 @@
 # Import everything from the games module
 from game import *
 
-print 'Hello world!'
+print 'PyBlackjack'
+print 'A small CLI-based Blackjack game simulator'
+print 'Copyright 2012 Aldo Rey Balagulan, Licensed under GNU GPL v3'
+
+option = ''
+while option != 'q' and option != 'Q':
+    option = raw_input('Options:\n\t[E]asy - 5000 chips\n\t' + 
+            '[N]ormal - 2000 chips\n\t' +
+            '[H]ard - 500 chips\n\t[R]ags to Riches - 1 chip\n\t' +
+            '[Q]uit\nOption: ')
+    if option == 'e' or option == 'E':
+        game = Game(5000)
+    elif option =='n' or option == 'N':
+        game = Game(2000)
+    elif option == 'h' or option == 'H':
+        game = Game(500)
+    elif option == 'r' or option == 'R':
+        game = Game(1)
